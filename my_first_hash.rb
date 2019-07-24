5990a16329fccf30e4702c8277abf09a6de9d2f9
Shahwar Spall <shevy.spall@gmail.com>
2019-07-24 17:40:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,17 +1,18 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash ={"name" => "shahwar"}
   
 end
 
 
 def shipping_manifest
+  the_manifest = {"whale bone corsets" => 5,"porcelain vases" => 2 , "oil paintings" => 3}
+end
+
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
 
-end
 
 def retrieval
   shipping_manifest = {
@@ -19,9 +20,10 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+retrieval[2]
 end
 
 def adding
